2381b19c4500c0e7dd64b222ce775843f1982ec8
TangYaNan <18839673302@163.com>
2018-10-12 23:07:38 +0800

    shopthree type

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ import Order from '../pages/Order/Order.vue';
 import Profile from '../pages/Profile/Profile.vue';
 import Login from '../pages/Login/Login.vue';
 
+import Shop from '../pages/Shop/Shop.vue';
+import ShopGoods from '../pages/Shop/ShopGoods/ShopGoods.vue';
+import ShopInfo from '../pages/Shop/ShopInfo/ShopInfo.vue';
+import ShopRatings from '../pages/Shop/ShopRatings/ShopRatings.vue';
+
 Vue.use(VueRouter);
 export default new VueRouter({
   //配置了所有的路由
@@ -19,34 +24,57 @@ export default new VueRouter({
       path: '/msite',
       component: MSite,
       meta: {
-        showFooter:true
+        showFooter: true
       }
     },
     {
       path: '/search',
       component: Search,
       meta: {
-        showFooter:true
+        showFooter: true
       }
     },
     {
       path: '/order',
       component: Order,
       meta: {
-        showFooter:true
+        showFooter: true
       }
     },
     {
       path: '/profile',
       component: Profile,
       meta: {
-        showFooter:true
+        showFooter: true
       }
     },
     {
       path: '/login',
-      component: Login
+      component: Login,
     },
+    {
+      path: '/shop',
+      component: Shop,
+      children: [
+        {
+          path: '/shop/ratings',
+          component: ShopRatings
+        },
+        {
+          path: '/shop/goods',
+          component: ShopGoods
+        },
+        {
+          path: '/shop/info',
+          component: ShopInfo
+        },
+        {
+          path: '',
+          redirect: '/shop/goods'
+        }
+      ]
+    },
+
     {//默认的跟陆有
       path: '/',
       redirect: '/msite'
